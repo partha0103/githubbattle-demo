afa98d547a6c02340de87280832d72d043780108
partha0103 <partha.sarathi@mountblue.io>
2017-08-13 11:38:56 +0530

    Configure webpack file

---
+++ app/components/App.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='container'>
+        <h1>Hello World</h1>
+      </div>
+    )
+  }
+}
+
+module.exports = App;
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './app/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'index_bundle.js'
+    },
+    module: {
+        rules: [
+            { test: /\.(js)$/, use: 'babel-loader'},
+            { test: /\.(css)$/, use: ['css-loader', 'style-loader']},
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: 'app/index.html'
+      })
+    ]
+}
