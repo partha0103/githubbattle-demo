d2016ea26ee6b5556b2ff1e4dd08b280adeb7d3e
partha0103 <partha.sarathi@mountblue.io>
2017-08-16 13:55:52 +0530

    Add util directory and make ajax call to fetch data

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,10 +1,40 @@
 let React = require('react');
+let PropTypes = require('prop-types');
+let api = require('../utils/api');
+
+function SelecteLanguage(props){
+    let languages = ['All', 'Javascript', 'Java', 'Python', 'CSS', 'Ruby'];
+    return(
+        <ul className="languages">
+            {
+            languages.map( (lang) => {
+                return(
+                        <li
+                            style = { lang === props.selectedLanguage ? {color: '#d0021b'}: null}
+                            onClick={props.onSelect.bind(null, lang)}
+                            key={lang}
+                        >
+                            {lang}
+                        </li>
+                    )
+                })
+            }
+        </ul>
+    )
+}
+
+
+SelecteLanguage.propTypes = {
+    selectedLanguage: PropTypes.string.isRequired,
+    onSelect: PropTypes.func.isRequired
+}
 
 class Popular extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            selectedLanguage: 'All'
+            selectedLanguage: 'All',
+            repos: null
         }
         this.updateLanguage = this.updateLanguage.bind(this);
     }
@@ -12,29 +42,33 @@ class Popular extends React.Component {
     updateLanguage(lang){
         this.setState(() => {
             return {
-                selectedLanguage: lang
+                selectedLanguage: lang,
+                repos: null
             }
         });
+
+        api.fetchPopularRepos(lang)
+            .then( (repos) => {
+                this.setState( () => {
+                    return {
+                        repos: repos
+                    }
+                })
+            })
+    }
+
+    componentDidMount(){
+        this.updateLanguage(this.state.selectedLanguage);
     }
 
     render(){
-        let languages = ['All', 'Javascript', 'Java', 'Python', 'CSS', 'Ruby'];
         return(
-            <ul className="languages">
-                {
-                    languages.map( (lang) => {
-                        return(
-                            <li
-                                style = { lang === this.state.selectedLanguage ? {color: '#d0021b'}: null}
-                                onClick={this.updateLanguage.bind(null, lang)}
-                                key={lang}
-                            >
-                                {lang}
-                            </li>
-                        )
-                    })
-                }
-            </ul>
+            <div>
+                <SelecteLanguage
+                    selectedLanguage = {this.state.selectedLanguage}
+                    onSelect = {this.updateLanguage}
+                />
+            </div>
         )
     }
 }
